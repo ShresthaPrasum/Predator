bcd1416f72d8446f62a50761779b298637b27bb5
prasum shrestha <prasumshrestha8877@gmail.com>
2026-05-10 09:50:05 +0545

    Building Timebomb mechanics

--- Assets/Scripts/timebomb.cs
+++ Assets/Scripts/timebomb.cs
@@ -1,8 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TimeBomb : MonoBehaviour
 {
-    public GameObject startPlayer;
+    public GameObject Bomb;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("BombTrigger"))
+        {         
+            Bomb.transform.SetParent(other.transform.parent);
+        }
+    }
 
-    
+
 }
