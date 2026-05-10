2803d98ade2284b0d403bdd3ccb2120a43d2f646
prasum shrestha <prasumshrestha8877@gmail.com>
2026-05-10 10:46:56 +0545

    WHY IT"S NOT WORKING!!!!

--- Assets/Scripts/timebomb.cs
+++ Assets/Scripts/timebomb.cs
@@ -3,14 +3,7 @@ using UnityEngine;
 
 public class TimeBomb : MonoBehaviour
 {
-    public GameObject Bomb;
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("BombTrigger"))
-        {         
-            Bomb.transform.SetParent(other.transform.parent);
-        }
-    }
+    
 
 
 }
